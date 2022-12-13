2c3e9e4314f9ef549cdcafaef8a198535445bc11
5amma5 <sam.anders5@gmail.com>
2022-12-12 21:01:53 -0700

    Created Airport and Flight models, created associations between both, migrated db, and created seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport.destroy_all
+Flight.destroy_all
+
+Airport.create!([{
+  name: "SLC"
+},
+{
+  name: "JFK"
+},
+{
+  name: "ORD"
+},
+{
+  name: "ATL"
+},
+{
+  name: "DFW"
+},
+{
+  name: "DEN"
+},
+{
+  name: "LAX"
+},
+{
+  name: "LAS"
+},
+{
+  name: "PHX"
+}])
+
+p "Created #{Airport.count} airports"
+
+25.times do |index|
+  rand_depart = Airport.ids.sample
+  rand_arrive = (Airport.ids - [rand_depart]).sample
+  Flight.create!(departure_id: rand_depart,
+                 arrival_id: rand_arrive,
+                 start: Date.today+rand(10000),
+                 duration: rand(60..360))
+end
+
+
+p "Created #{Flight.count} flights"
