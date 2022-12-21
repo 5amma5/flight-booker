0e7add93f561c7aed1e13b2c5dc550104d812503
5amma5 <sam.anders5@gmail.com>
2022-12-20 18:11:21 -0700

    setup bookings#new form to accept nested passengers. added bookings#create action that creates booking object from booking#new form. setup bookings#show view to display a created bookings flight and passenger information.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,29 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @flight = Flight.find(params[:flight])
     @passenger_count = params[:passenger_count].to_i
-    @flight_id = params[:flight]
     @passenger_count.times { @booking.passengers.build }
   end
 
   def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      flash[:success] = "Your flight is booked!"
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:first_name, :last_name, :email])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
-  get 'bookings/create'
   root "flights#index"
   resources :flights, only: [:index]
+  resources :bookings, only: [:new, :create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
