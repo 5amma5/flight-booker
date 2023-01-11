f4f782c6bbf521572e3fce43579ed55c77687d71
5amma5 <32971882+5amma5@users.noreply.github.com>
2023-01-11 14:38:20 -0700

    refactored/cleaned up code

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,20 +1,27 @@
 class FlightsController < ApplicationController
-  def index
-    @airport_options = Airport.all.order(:name).map { |a| [a.name, a.id] }
-    # grab only start:datetime and put into array > convert each datetime to date > remove duplicate date entries
-    # in array > create new nested array that is ideal for Views "select" method
-    @date_options = Flight.pluck(:start).map(&:to_date).uniq.sort.map { |d| [d.strftime('%m/%d/%Y'), d] }
-    @flight = Flight.new
+  before_action :fetch_search_results, only: [:index]
 
-    unless params[:flight].nil?
-      date = Date.parse(search_params[:start])
-      @flight_search_results = Flight.where("departure_id = ? AND arrival_id = ?", search_params[:departure_id], search_params[:arrival_id]).where(start: date.all_day).order(:start)
-    end
+  def index
+    # grabs all airport records and puts name and id's into a 2d array i.e: [[airport.name1, airport.id], [airport.name2, airport.id], etc..]]
+    @airport_options = Airport.pluck(:name, :id).sort
+
+    # grab only start:datetime and put into array > converts from datetime to date > removes duplicate date
+    # entries > creates new nested array that is ideal for Views "select" method
+    @date_options = Flight.pluck(:start).map(&:to_date).uniq.sort.map { |d| [d.strftime('%m/%d/%Y'), d] }
   end
 
   private
 
+  def fetch_search_results
+    # want this to run ONLY if search params have been provided. aka run if form has been submitted. If not, return
+    return if search_params.empty?
+
+    @flight_search_results = Flight.where(departure_id: search_params[:departure_id],
+                                          arrival_id: search_params[:arrival_id])
+                                   .where(start: Date.parse(search_params[:start]).all_day).order(:start)
+  end
+
   def search_params
-    params.require(:flight).permit(:departure_id, :arrival_id, :start, :passenger_count)
+    params.permit(:departure_id, :arrival_id, :start, :passenger_count)
   end
 end
