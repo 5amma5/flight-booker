9bad4dd02a2d48f1785b1823864b9f7a5b7ba02c
5amma5 <32971882+5amma5@users.noreply.github.com>
2023-01-11 14:38:21 -0700

    Updated flights results form to send params to booking#new. Set up booking#new action to receive params.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,9 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @passenger_count = params[:passenger_count].to_i
+    @flight_id = params[:flight]
+    @passenger_count.times { @booking.passengers.build }
   end
 
   def create
