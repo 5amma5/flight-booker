ec3f532386b319cae1e08f5764defa23f82e80f0
5amma5 <32971882+5amma5@users.noreply.github.com>
2023-01-11 14:38:21 -0700

    Created Booking & Passenger models and migrated db. Setup associations between them and Flight.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -5,4 +5,6 @@ class Flight < ApplicationRecord
                                  foreign_key: 'departure_id'
   belongs_to :arrival_airport, class_name: 'Airport',
                                foreign_key: 'arrival_id'
+  has_many :bookings
+  has_many :passengers, through: :bookings
 end
